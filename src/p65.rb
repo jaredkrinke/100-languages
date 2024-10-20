5c67edcdb1610edbf9f0d3e373e469e830b9abb3
Jared Krinke <jared.krinke@gmail.com>
2024-10-20 08:16:10 -0700

    Fix whitespace

--- src/p65.rb
+++ src/p65.rb
@@ -1,29 +1,28 @@
 def eTerms()
-    Enumerator.new do |g|
-      [2, 1, 2].each do |a| g.yield a end
-      (2..).each do |k|
-        [1, 1, k * 2].each do |a| g.yield a end
-      end
+  Enumerator.new do |g|
+    [2, 1, 2].each do |a| g.yield a end
+    (2..).each do |k|
+      [1, 1, k * 2].each do |a| g.yield a end
     end
   end
-  
-  def expandFraction(terms)
-    r = nil
-    terms.reverse.each do |a|
-      r = Rational(a) + (r ? 1/r : 0)
-    end
-    r
+end
+
+def expandFraction(terms)
+  r = nil
+  terms.reverse.each do |a|
+    r = Rational(a) + (r ? 1/r : 0)
   end
-  
-  def solve(n)
-    numerator = expandFraction(eTerms.take(n)).numerator
-    sum = 0
-    while numerator > 0 do
-      sum += numerator % 10
-      numerator /= 10
-    end
-    sum
+  r
+end
+
+def solve(n)
+  numerator = expandFraction(eTerms.take(n)).numerator
+  sum = 0
+  while numerator > 0 do
+    sum += numerator % 10
+    numerator /= 10
   end
-  
-  puts(solve(100))
-  
+  sum
+end
+
+puts(solve(100))
